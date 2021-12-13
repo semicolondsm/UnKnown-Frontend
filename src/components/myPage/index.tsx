1dd981d3c0a0e2bb71362b5d1abff5a737f611a9
추혜연 <hyeyoenchurros@naver.com>
2021-12-13 20:12:25 +0900

    ⚡ :: (#34) mypage index 파일 수정

--- src/components/myPage/index.tsx
+++ src/components/myPage/index.tsx
@@ -5,7 +5,28 @@ import * as S from './style';
 interface MainPageProps {}
 
 const MainPage: React.FC<MainPageProps> = () => {
-  return <></>;
+  return (
+    <>
+      <Header></Header>
+      <S.MyPageWrapper>
+        <S.MyPageText>마이페이지</S.MyPageText>
+        <S.MyPageLine></S.MyPageLine>
+        <S.ProfileBox>
+          <S.ProfileImage></S.ProfileImage>
+          <S.NameId>
+            <S.ProfileName>hyeyeonchurros</S.ProfileName>
+            <S.ProfileBox>
+              <S.ProfileId>qwer1234</S.ProfileId>
+              <S.ChangePassword>비밀번호 변경</S.ChangePassword>
+            </S.ProfileBox>
+          </S.NameId>
+        </S.ProfileBox>
+        <S.MyPostText>내 게시물</S.MyPostText>
+        <Post></Post>
+        <Post></Post>
+      </S.MyPageWrapper>
+    </>
+  );
 };
 
 export default MainPage;
