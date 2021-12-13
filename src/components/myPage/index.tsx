19402c07578dca11282a2b34cbd1e57ba6edda49
추혜연 <hyeyoenchurros@naver.com>
2021-12-13 20:08:40 +0900

    ⚡ :: (#34) mypage index 파일 작성

---
+++ src/components/myPage/index.tsx
@@ -0,0 +1,11 @@
+import Header from '../header';
+import Post from '../post';
+import * as S from './style';
+
+interface MainPageProps {}
+
+const MainPage: React.FC<MainPageProps> = () => {
+  return <></>;
+};
+
+export default MainPage;
